11e0e1c162658dd866ccdd785416872d5254ea77
Trainer <admin@example.com>
2024-03-25 11:51:25 +0000

    showcase css modules.

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -6,6 +6,8 @@ import { useThemeContext } from "@/app/context/theme";
 import { AppState } from "@/lib/store";
 // import useCart from "@/app/hooks/useCart";
 
+import styles from "./styles.module.scss"
+
 const Header = () => {
 
     const items = useSelector((store: AppState) => {
@@ -17,7 +19,7 @@ const Header = () => {
     // const { items } = useCart()
     const { toggleTheme, theme } = useThemeContext()
 
-    return <header className={`${theme === "light" ? "bg-white text-black" : "bg-black text-white"}`}>
+    return <header className={`${theme === "light" ? "bg-white text-black" : "bg-black text-white"} ${styles.form}`}>
         Music Store
         Cart : {items.length}
         <button onClick={toggleTheme}>{theme}</button>
--- app/components/login/index.tsx
+++ app/components/login/index.tsx
@@ -2,6 +2,8 @@
 import { useState } from "react";
 import { Formik } from "formik";
 import * as Yup from "yup";
+import styles from "./design.module.css";
+import Header from "../header";
 
 const formValidateSchema = Yup.object().shape({
   password: Yup.string().required("Password is a required field"),
@@ -46,54 +48,60 @@ const LoginForm = ({
         console.log(touched);
 
         return (
-          <form className="p-1" onSubmit={handleSubmit}>
-            <p className="p-4">
-              <label htmlFor="email">Email</label>
-              <input
-                type="text"
-                name="contact.email"
-                value={email}
-                onChange={handleChange}
-                onBlur={handleBlur}
-              />
-              {errors?.contact?.email && touched.contact?.email && (
-                <span>{errors?.contact?.email}</span>
-              )}
-            </p>
-            <p className="p-4">
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                name="password"
-                value={password}
-                onChange={handleChange}
-                onBlur={handleBlur}
-              />
-              {errors?.password && touched.password && (
-                <span>{errors?.password}</span>
-              )}
-            </p>
+          <>
+            <form className={`p-1 m-1 ${styles.loginForm} ${styles.colorBlue}`} onSubmit={handleSubmit}>
+              <p className="p-4">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="text"
+                  name="contact.email"
+                  value={email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors?.contact?.email && touched.contact?.email && (
+                  <span>{errors?.contact?.email}</span>
+                )}
+              </p>
+              <p className="p-4">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  value={password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors?.password && touched.password && (
+                  <span>{errors?.password}</span>
+                )}
+              </p>
 
-            <p className="p-4">
-              <label htmlFor="password">Username</label>
-              <input type="password" name="username" onChange={handleChange} />
-            </p>
+              <p className="p-4">
+                <label htmlFor="password">Username</label>
+                <input
+                  type="password"
+                  name="username"
+                  onChange={handleChange}
+                />
+              </p>
 
-            <p className="p-4">
-              <label htmlFor="password">Phone no</label>
-              <input
-                value={phoneno}
-                type="password"
-                name="contact.phoneno"
-                onChange={handleChange}
-                onBlur={handleBlur}
-              />
-              {errors?.contact?.phoneno && touched.contact?.phoneno && (
-                <span>{errors?.contact?.phoneno}</span>
-              )}
-            </p>
-            <button type="submit">Login</button>
-          </form>
+              <p className="p-4">
+                <label htmlFor="password">Phone no</label>
+                <input
+                  value={phoneno}
+                  type="password"
+                  name="contact.phoneno"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors?.contact?.phoneno && touched.contact?.phoneno && (
+                  <span>{errors?.contact?.phoneno}</span>
+                )}
+              </p>
+              <button type="submit">Login</button>
+            </form>
+          </>
         );
       }}
     </Formik>
